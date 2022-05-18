8adb662abace92251e3271475776ff02aabf4bef
CharlesD38 <90854764+CharlesD38@users.noreply.github.com>
2022-05-17 20:21:38 -0400

    Having trouble converting looking at all latex and markdown

---
+++ Where Convert.py
@@ -0,0 +1,25 @@
+
+from traitlets.config import Config
+import nbformat as nbf
+from nbconvert.exporters import PDFExporter
+from nbconvert.preprocessors import TagRemovePreprocessor
+
+# Setup config
+c = Config()
+
+c.TagRemovePreprocessor.remove_input_tags = ('hide_input',)
+c.TagRemovePreprocessor.enabled = True
+# Configure and run out exporter
+c.PDFExporter.preprocessors = ["nbconvert.preprocessors.TagRemovePreprocessor"]
+
+exporter = PDFExporter(config=c)
+exporter.register_preprocessor(TagRemovePreprocessor(config=c),True)
+
+# Configure and run our exporter - returns a tuple - first element with html,
+# second with notebook metadata
+output = PDFExporter(config=c).from_filename(
+        r"C:\MFE\Github\finops_casestudy\Bond Portfolio Optimization Case Study.ipynb")
+
+# Write to output html file
+with open(r"C:\MFE\Github\finops_casestudy\Bond Portfolio Optimization Case Study.pdf",  "wb") as f:
+    f.write(output[0])
